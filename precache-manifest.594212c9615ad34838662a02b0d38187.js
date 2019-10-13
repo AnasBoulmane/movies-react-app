c34ad7d7b8aaf22b597578616f9ff90f607fe55e
aboulmane <aboulmane@sqli.com>
2019-10-13 16:32:03 +0100

    Updates

---
+++ precache-manifest.594212c9615ad34838662a02b0d38187.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da517b31b606d53e50dbf66d43efd9ff",
+    "url": "/movies-react-app/index.html"
+  },
+  {
+    "revision": "524524888524c3e5f79d",
+    "url": "/movies-react-app/static/css/main.c632ea61.chunk.css"
+  },
+  {
+    "revision": "9b1b4aba079859537ed1",
+    "url": "/movies-react-app/static/js/2.487a6132.chunk.js"
+  },
+  {
+    "revision": "524524888524c3e5f79d",
+    "url": "/movies-react-app/static/js/main.66bcad3e.chunk.js"
+  },
+  {
+    "revision": "03e20282fb17923065e0",
+    "url": "/movies-react-app/static/js/runtime-main.27d0ddaf.js"
+  }
+]);
